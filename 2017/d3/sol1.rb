3559f608801c4cefa06e7f63411b586cb4fea5f9
Perry Shuman <thenoirbot@gmail.com>
2017-12-04 16:21:29 -0600

    Day3 17 p1

---
+++ 2017/d3/sol1.rb
@@ -0,0 +1,14 @@
+input = 289326
+ring = 0
+
+def ring_end(count)
+  return (1 + (2*count))**2
+end
+
+while input > ring_end(ring)
+  ring = ring + 1
+end
+
+depth = ring_end(ring) - input
+
+puts (ring * 2) - (depth % (ring*2))
